ada1e0f0e230d34d0eee4c0b58361938ac5fa803
yijaejun <yijaejun81@gmail.com>
2018-06-05 16:33:27 +0900

    complete the function that one can merge rows selected in the same column

--- script.js
+++ script.js
@@ -52,7 +52,13 @@
 		if(_self.attr('rowspan') > 1){
 			console.log('병합된 셀이 : ' +  _self.attr('rowspan'));
 
-			for(var i=row,size=_self.attr('rowspan');i<size;i++){
+			// 상단의 코드와 중복되는 지점
+			console.log('두번째로 선택된 셀이 병합된 셀일 경우');
+			console.log('row ' + row);
+
+			var i=row, size=parseInt(_self.attr('rowspan')) + parseInt(row);
+
+			for(;i<size;i++){
 				tmp_storage.push({
 					c: column,
 					r: i
@@ -76,12 +82,46 @@
 		}
 		
 
-		// 병합된 셀을 선택 후에 바로 위의 칼럼을 선택할 경우 문제가 된다.
-		// 따라서 아래의 로직을 변경해야 한다.
-		if(Math.abs(tmp_storage[tmp_storage.length-2].r - row) > 1){
-			cancelSelected('연속된 영역을 선택해야 합니다.');	
-			return;
-		} 
+		// TODO 병합된 셀을 선택 후에 바로 위의 칼럼을 선택할 경우 문제가 된다.
+		
+		// if(Math.abs(tmp_storage[tmp_storage.length-2].r - row) > 1){
+		// 	cancelSelected('연속된 영역을 선택해야 합니다.');	
+		// 	return;
+		// } 
+
+		// 중간에 건너띄어서 셀이 선택이 되지 않아야 한다.
+		// 선택영역간에 rowspan이라는 것이 있고 그걸 통해서 계산한 결과 연속된 지점이라도 판단이 된다면 ok
+		// 만약 연속된 구간이 아니라고 판단이 되면 모두 선택 해제
+		// 그럼 연속된 구간이라는 것을 어떻게 판단을 할 수가 있을 것인가???
+
+		
+		
+		// 순서대로 정렬을 한 후에 중간에 순서가 빠져 있는지 확인하고 순서가 빠진 경우 전체 선택을 해제하고 배열을 비운다.
+		// 그럼 일단 r만 따로 배열에 정리하여 넣은 뒤에 sort()로 정렬하고 순차적으로 이동하면서 빈 구석이 있는지 확인한다.
+		var tmp_single_row = [];
+		for(var i=0,size=tmp_storage.length;i<size;i++){
+			tmp_single_row.push(tmp_storage[i].r);
+		}
+
+		tmp_single_row = tmp_single_row.sort();
+
+		console.log('총 선택된 영역이 중간이 빠진 것이 없는지 확인하기 위해서');
+		console.log(tmp_single_row);
+		
+		var tmp_selected_value = null;
+		for(var i=0,size=tmp_single_row.length;i<size;i++){
+			if(tmp_selected_value == null){
+				tmp_selected_value = tmp_single_row[i];
+			}
+
+			if(Math.abs(tmp_single_row[i] - tmp_selected_value) > 1){
+				console.log('순차적이지 않으므로 모두 해제!!');
+				cancelSelected('선택된 영역이 잘못되었습니다.');
+				return;
+			}
+			tmp_selected_value = tmp_single_row[i];
+		}
+
 		
 		console.log(column + ' / ' + row);
 
